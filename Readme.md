d9ae602059c51f07fcfa4c3cd9532078857e02bf
Dolar, Ziga <ziga.dolar@here.com>
2020-01-23 10:46:29 +0100

    Add more flexibility to fetcher script, also add option to get pipeline/jobs info

--- Readme.md
+++ Readme.md
@@ -18,16 +18,18 @@ python -m SimpleHTTPServer
 In the other terminal window, start the updater for the projects, whose pipelines you want to monitor
 
 ```sh
-./fetcher.sh {{gitlab_host_url}} {{your_gitlab_api_token}} {{comma_separated_list_of_project_ids}} {{optional_git_ref_name}}
+./fetcher.sh --host {{gitlab_host_url}} --token {{your_gitlab_api_token}} --projects {{comma_separated_list_of_project_ids}} --ref {{optional_git_ref_name}} --variables {{optional_flag_if_should_fetch_variables}} --jobs {{optional_flag_if_should_fetch_jobs}}
 ```
 
 Example:
 
 ```sh
-./fetcher.sh https://gitlab.example.com someTokenString 123,456 master
+./fetcher.sh --host https://gitlab.example.com --token someTokenString --projects 123,456 --ref master --variables false --jobs true
 ```
 
-> If no `branch` is provided, all branches will be used.
+> If no `--ref` is provided, all branches will be used.
+> If `--variables` is not provided, or `false`, no variables information will be fetched
+> If `--jobs` is not provided, or `false`, no jobs information will be fetched
 
 <br />
 
@@ -40,7 +42,9 @@ Accepted query parameters:
 - `projectId`: __required__, id of the gitlab project
 - `showVariables`: *optional*, a flag to determine, if a column with pipeline variables should be shown - if `1`, the column will be shown
 - `variables`: *optional*, a comma-separated list of variable names, that should be shown
+- `showJobs`: *optional*, a flag to determine, if a column with pipeline jobs should be shown - if `1`, the column will be shown
+- `jobs`: *optional*, a comma-separated list of job names, that should be shown
 
 Example:
 
-`localhost:8000/?projectId=123&showVariables=1&variables=SOME_VARIABLE,OTHER_VARIABLE`
+`localhost:8000/?projectId=123&showVariables=1&variables=SOME_VARIABLE,OTHER_VARIABLE&showJobs=1&jobs=testJob`
--- fetcher.sh
+++ fetcher.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-gitlab_host="${1}"
-shift
-token="${1}"
-shift
-project_ids="${1}"
-shift
-ref="${1}"
+# get arguments from command line
+# currently supported:
+# `--host https://gitlab.example.com` 
+# `--token someTokenString`
+# `--projects 123,456`
+# `--ref some_branch`
+# `--variables true/false`
+# `--jobs true/false`
 
-if ! [[ ${gitlab_host} ]]; then
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+    fi
+    shift
+done
+
+if ! [[ ${host} ]]; then
   echo "Missing Gitlab host url"
   exit 1
 fi
@@ -18,7 +27,7 @@ if ! [[ ${token} ]]; then
   exit 1
 fi
 
-if ! [[ ${project_ids} ]]; then
+if ! [[ ${projects} ]]; then
   echo "Missing project id(s)"
   exit 1
 fi
@@ -29,15 +38,33 @@ else
   REF=""
 fi
 
+GET_VARIABLES=false
+
+if [[ $variables ]]; then
+  if [[ $variables = "true" ]]
+    then
+        GET_VARIABLES=true
+    fi
+fi
+
+GET_JOBS=false
+
+if [[ $jobs ]]; then
+  if [[ $jobs = "true" ]]
+    then
+        GET_JOBS=true
+    fi
+fi
+
 rm -rf data
 mkdir -p data
 
 PROJ_FILE="data/projects.json"
 
 echo "{" > $PROJ_FILE
-for project_id in ${project_ids//,/ }; do
+for project_id in ${projects//,/ }; do
   echo "Fetching project info for ${project_id}"
-  js=$(curl -sS --header "PRIVATE-TOKEN: ${token}" "${gitlab_host}/api/v4/projects/${project_id}")
+  js=$(curl -sS --header "PRIVATE-TOKEN: ${token}" "${host}/api/v4/projects/${project_id}")
   rx='"name_with_namespace":"([^"]*)"'
   if [[ "$js" =~ $rx ]]; then
     result="${BASH_REMATCH[1]}"
@@ -50,11 +77,11 @@ echo "}" >> $PROJ_FILE
 mkdir -p data/projects
 while true
 do
-  for project_id in ${project_ids//,/ }; do
+  for project_id in ${projects//,/ }; do
     echo "Fetching pipelines for ${project_id}"
     project_dir="data/projects/${project_id}"
     project_json="${project_dir}.json"
-    curl -sS --header "PRIVATE-TOKEN: ${token}" "${gitlab_host}/api/v4/projects/${project_id}/pipelines?order_by=updated_at${REF}" -o ${project_json}
+    curl -sS --header "PRIVATE-TOKEN: ${token}" "${host}/api/v4/projects/${project_id}/pipelines?order_by=updated_at${REF}" -o ${project_json}
 
     pipelines_dir="${project_dir}/pipelines"
     mkdir -p ${pipelines_dir}
@@ -69,18 +96,35 @@ do
 
     pipeline_ids=$(grep -${grep_param}o '"id":\d*?[^\\],' ${project_json})
 
-    for pipeline_id in ${pipeline_ids//,/ }; do
-      id=$(echo ${pipeline_id} | sed 's/"id":\(\d*\)/\1/')
+    if [[ $GET_VARIABLES = true ]]; then
+      for pipeline_id in ${pipeline_ids//,/ }; do
+        id=$(echo ${pipeline_id} | sed 's/"id":\(\d*\)/\1/')
 
-      pipeline_dir="${pipelines_dir}/${id}"
-      mkdir -p ${pipeline_dir}
+        pipeline_dir="${pipelines_dir}/${id}"
+        mkdir -p ${pipeline_dir}
 
-      variables_json="${pipeline_dir}/variables.json"
-      if ! [[ -f "$variables_json" ]]; then
-        echo "Fetching variables for pipeline ${id} for project ${project_id}"
-        curl -sS --header "PRIVATE-TOKEN: ${token}" "${gitlab_host}/api/v4/projects/${project_id}/pipelines/${id}/variables" -o ${variables_json}
-      fi
-    done
+        variables_json="${pipeline_dir}/variables.json"
+        if ! [[ -f "$variables_json" ]]; then
+          echo "Fetching variables for pipeline ${id} for project ${project_id}"
+          curl -sS --header "PRIVATE-TOKEN: ${token}" "${host}/api/v4/projects/${project_id}/pipelines/${id}/variables" -o ${variables_json}
+        fi
+      done
+    fi
+
+    if [[ $GET_JOBS = true ]]; then
+      for pipeline_id in ${pipeline_ids//,/ }; do
+        id=$(echo ${pipeline_id} | sed 's/"id":\(\d*\)/\1/')
+
+        pipeline_dir="${pipelines_dir}/${id}"
+        mkdir -p ${pipeline_dir}
+
+        jobs_json="${pipeline_dir}/jobs.json"
+        if ! [[ -f "$jobs_json" ]]; then
+          echo "Fetching jobs for pipeline ${id} for project ${project_id}"
+          curl -sS --header "PRIVATE-TOKEN: ${token}" "${host}/api/v4/projects/${project_id}/pipelines/${id}/jobs" -o ${jobs_json}
+        fi
+      done
+    fi
   done
 
   echo "Sleeping ..."
